74021d5e353004138a70e7caa3445dad0c23798b
Eden Natanzon <eden.natanzon@gmail.com>
2023-03-12 19:45:23 +0200

    1. Added:          /getAllChargingStationsLocations endpoint. 2. Added:          postMan /getAllChargingStationsLocations.

--- server/src/main/java/com/server/chargingStations/ChargingStationController.java
+++ server/src/main/java/com/server/chargingStations/ChargingStationController.java
@@ -1,17 +1,22 @@
 package com.server.chargingStations;
 
+import com.server.GeoLocation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/chargingStations")
 public class ChargingStationController {
-
     @Autowired
     private ChargingStationRepository m_chargingStationsRepository;
+    @Autowired
+    private MongoTemplate m_mongoTemplate;
 
     @PostMapping("/createChargingStation")
     public void createChargingStation(@RequestBody ChargingStation chargingStation) {
@@ -21,6 +26,15 @@ public class ChargingStationController {
     // TODO E - DELETE ChargingStation
 
     // TODO E - GET getAllChargingStationsLocations
+    @GetMapping(value = "/getAllChargingStationsLocations", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<GeoLocation> getAllChargingStationsLocations() {
+        List<GeoLocation> locations = m_mongoTemplate.query(ChargingStation.class)
+                .distinct("location")
+                .as(GeoLocation.class)
+                .all();
+        return locations;
+    }
 
     // TODO E - Charge
 
