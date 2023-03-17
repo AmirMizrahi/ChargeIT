ac0cd72b16bdf7fe87f2c6677baac5f1a1be5875
Eden Natanzon <eden.natanzon@gmail.com>
2023-03-17 15:55:40 +0200

    Added: charge and unCharge.

--- server/src/main/java/com/server/chargingStations/ChargingStationRepository.java
+++ server/src/main/java/com/server/chargingStations/ChargingStationRepository.java
@@ -1,6 +1,10 @@
 package com.server.chargingStations;
 
+import com.server.GeoLocation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ChargingStationRepository extends MongoRepository<ChargingStation, String> {
+    Optional<ChargingStation> findByLocation(GeoLocation location);
 }
