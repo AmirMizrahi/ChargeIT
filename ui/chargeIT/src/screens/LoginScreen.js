477e848f5f802fe112f83ed7e07bcdb19030a58d
Amir <amirmiz33@gmail.com>
2023-03-21 19:03:31 +0200

    fixed login

--- ui/chargeIT/src/screens/LoginScreen.js
+++ ui/chargeIT/src/screens/LoginScreen.js
@@ -1,10 +1,29 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from "react-native";
 import {Button, Input} from "react-native-elements";
+import basicApi from "../api/basicApi";
+import {logger} from "react-native-logs";
 
 const LoginScreen = () => {
+    const log = logger.createLogger();
+
     const [userName,setUserName] = useState('');
     const [password,setPassword] = useState('');
+
+    const login = async () => {
+
+        try{
+            log.debug(`Username=${userName} trying to login.`);
+            const response = await basicApi.post('/users/login',
+                {userName,password})
+            log.debug(`Received code ${response.status}. Successfully registered Username=${userName}.`);
+            log.debug(response.data);
+        }
+        catch (e) {
+            log.error("Something went wrong :(");
+            log.error(e);
+        }
+    }
 
     return (<View>
         <Text>Login Screen</Text>
@@ -21,7 +40,7 @@ const LoginScreen = () => {
             />
             <Button
                 title = "Login"
-                /*onPress={() => signup ({email, password})}*//>
+                onPress={() => login ({userName, password})}/>
     </View>
     );
 };
