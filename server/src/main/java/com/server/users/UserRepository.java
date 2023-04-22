d0bf93a36dc5f6b15b1f85ca677f7c5e4723e918
Eden Natanzon <eden.natanzon@gmail.com>
2023-04-22 21:31:20 +0300

    Add: EmailAndPassword. Remove: userName, updateUserEmail(); Delete: UserNameAndPassword Class.

--- server/src/main/java/com/server/users/UserRepository.java
+++ server/src/main/java/com/server/users/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUserName(String userName);
+    Optional<User> findByEmail(String email);
 
 }
