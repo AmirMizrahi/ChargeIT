01c3b0e92ed9c1d033b5dbeb91dc30ffb07663ae
Eden Natanzon <eden.natanzon@gmail.com>
2023-05-01 15:50:54 +0300

    Updated: PostMan. Added: - updatePricePerVolt.        - updateUserPassword.        - updateUserFirstName.        - updateUserLastName.

--- server/src/main/java/com/server/users/User.java
+++ server/src/main/java/com/server/users/User.java
@@ -49,8 +49,16 @@ public class User {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
