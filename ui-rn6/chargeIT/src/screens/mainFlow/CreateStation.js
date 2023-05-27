4e4b55ddbb31acaf03673efb068412a13e3ba7db
Amir <amirmiz33@gmail.com>
2023-05-28 00:43:44 +0300

    edited style for station creation

--- ui-rn6/chargeIT/src/screens/mainFlow/CreateStation.js
+++ ui-rn6/chargeIT/src/screens/mainFlow/CreateStation.js
@@ -1,139 +1,124 @@
-import React, {useContext, useState} from "react";
-import {View, Text, StyleSheet} from "react-native";
-import {Picker} from "@react-native-picker/picker";
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import Buttons from "../../components/Buttons";
-import Map from "../../components/Map";
-import MapView, {Marker} from "react-native-maps";
-import {TextInput} from "react-native-gesture-handler";
+import { TextInput } from "react-native-gesture-handler";
 import Spacer from "../../components/Spacer";
-import trackMyLocation from "../../hooks/trackMyLocation";
 
-import {useIsFocused} from "@react-navigation/native";
-import {Context as StationsContext} from "../../context/StationsContext";
+import { Entypo } from "@expo/vector-icons";
+import { Context as StationsContext } from "../../context/StationsContext";
 
-const CreateStation = ({navigation, route}) => {
-    const {getCurrentLocation, createChargingStation} =
-        useContext(StationsContext);
-    const [err] = trackMyLocation(useIsFocused(), getCurrentLocation);
-    const [pricePerVolt, setPrice] = useState("");
-    const [stationName, setName] = useState("");
-    const [chargerType, setSelectedValue] = useState("TYPE_0");
-    const {latitude, longitude} = route.params;
+const CreateStation = ({ navigation, route }) => {
+  const { createChargingStation } = useContext(StationsContext);
+  const [pricePerVolt, setPrice] = useState("");
+  const [stationName, setName] = useState("");
+  const [chargerType, setSelectedValue] = useState("TYPE_0");
+  const { latitude, longitude } = route.params;
 
-    // const handleMapPress = (event) => {
-    //   const { coordinate } = event.nativeEvent;
-    //   setLocation(coordinate);
-    // };
-
-    // const handleSavePress = () => {
-    //   // Handle save location here
-    //   console.log("Selected location:", location);
-    // };
-
-    return (
-        <View style={styles.mainView}>
-            <Text style={styles.headerText}>Create New Charging Station</Text>
-
-            <View style={{borderColor: "red", borderWidth: 3}}>
-                <Text>
-                    {latitude},{longitude}
-                </Text>
-                <View style={{display: 'flex', flexDirection: 'row'}}>
-                    <Text>Name: </Text>
-                    <TextInput
-                        editable
-                        maxLength={40}
-                        onChangeText={text => setName(text)}
-                        value={stationName}
-                        style={{padding: 10, borderWidth: 1, width: 150}}
-                    />
-                </View>
-                <View style={{display: 'flex', flexDirection: 'row'}}>
-                    <Text>Price per volt: </Text>
-                    <TextInput
-                        editable
-                        keyboardType='number-pad'
-                        maxLength={5}
-                        onChangeText={text => setPrice(text)}
-                        value={pricePerVolt}
-                        style={{padding: 10, borderWidth: 1, width: 150}}
-                    />
-                </View>
-
-                <View style={styles.viewGeneral}>
-                    <Text style={styles.label}>pricePerVolt:</Text>
-                    <TextInput style={styles.value} onChangeText={setPrice}></TextInput>
-                </View>
-
-                <View style={styles.viewGeneral}>
-                    <Text style={styles.label}>chargerType:</Text>
-                    <View style={{borderColor: "gray", borderStyle: 'bold', borderWidth: 2, height: 80}}>
-                        <Picker
-                            style={{height: 150, width: 200}}
-                            itemStyle={{height: 100, width: 200, fontSize: 14}}
-                            selectedValue={chargerType}
-                            onValueChange={(itemValue, itemIndex) =>
-                                setSelectedValue(itemValue)
-                            }
-                        >
-                            <Picker.Item label="Type 0" value="TYPE_0"/>
-                            <Picker.Item label="Type 1" value="TYPE_1"/>
-                        </Picker>
-                        <Buttons
-                            on_press={() => {
-                                createChargingStation({
-                                    latitude,
-                                    longitude,
-                                    pricePerVolt,
-                                    chargerType,
-                                    stationName
-                                });
-                                navigation.navigate('Stations');
-                            }}
-
-                            btn_text={"Create"}
-                        ></Buttons>
-                    </View>
-                </View>
-            </View>
+  return (
+    <View style={styles.mainView}>
+      <Text style={styles.headerText}>Create New Charging Station</Text>
+      <Spacer></Spacer>
+      <View>
+        <View style={styles.lineTextAndInput}>
+          <Entypo name="add-user" size={24} color="black" />
+          <Text style={styles.label}> Station Name: </Text>
+          <TextInput
+            editable
+            maxLength={40}
+            onChangeText={setName}
+            value={stationName}
+            style={styles.input}
+          />
         </View>
-    );
+        <View style={styles.lineTextAndInput}>
+          <Entypo name="price-tag" size={24} color="black" />
+          <Text style={styles.label}> Price per Volt: </Text>
+          <TextInput
+            editable
+            keyboardType="number-pad"
+            maxLength={5}
+            onChangeText={setPrice}
+            value={pricePerVolt}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.lineTextAndInput}>
+          <Entypo name="battery" size={24} color="black" />
+          <Text style={styles.label}> Type of Charger:</Text>
+          <Picker
+            style={styles.input}
+            selectedValue={chargerType}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedValue(itemValue)
+            }
+          >
+            <Picker.Item label="Type 0" value="TYPE_0" />
+            <Picker.Item label="Type 1" value="TYPE_1" />
+          </Picker>
+        </View>
+        <Buttons
+          on_press={() => {
+            createChargingStation({
+              latitude,
+              longitude,
+              pricePerVolt,
+              chargerType,
+              stationName,
+            });
+            navigation.navigate("Stations");
+          }}
+          btn_text={"Create"}
+        ></Buttons>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-
-    headerText: {
-        fontSize: 18,
-        fontWeight: "700",
-        marginTop: 16,
-    },
-    viewGeneral: {
-        flexDirection: "row",
-    },
-    buttons: {
-        alignItems: "center",
-    },
-    smallText: {
-        maxWidth: "50%",
-        color: "#999",
-        fontSize: 14,
-        textAlign: "center",
-        paddingTop: 10,
-    },
-    mainView: {
-        alignItems: "center",
-        justifyContent: "center",
-        flex: 1,
-        marginTop: 100
-    },
-    picker: {
-        borderColor: "blue",
-        borderWidth: 3,
-        fontSize: 20,
-        justifyContent: "center",
-        flex: 1,
-        height: 20
-    }
+  headerText: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginTop: 16,
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  buttons: {
+    alignItems: "center",
+  },
+  smallText: {
+    maxWidth: "50%",
+    color: "#999",
+    fontSize: 14,
+    textAlign: "center",
+    paddingTop: 10,
+  },
+  mainView: {
+    marginLeft: 20,
+  },
+  picker: {
+    fontSize: 20,
+    justifyContent: "center",
+    flex: 1,
+    height: 20,
+  },
+  input: {
+    width: "60%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    paddingLeft: 10,
+  },
+  lineTextAndInput: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  label: {
+    fontSize: 18,
+  },
 });
 
 export default CreateStation;
