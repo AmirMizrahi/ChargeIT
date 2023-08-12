22e896f702feb14c9d0ce02e073c9c73a7667e73
Amir <amirmiz33@gmail.com>
2023-08-12 17:03:41 +0300

    added delete button without logic

---
+++ ui-rn6/chargeIT/src/components/MyStationCard.js
@@ -0,0 +1,158 @@
+import {Text, View, StyleSheet, Button, Dimensions} from "react-native";
+import {FontAwesome5} from '@expo/vector-icons';
+import StarRating from 'react-native-star-rating-widget';
+import React, {useState} from "react";
+import Popup from "./Popup";
+import QRCode from "react-native-qrcode-svg";
+import MapView, {Marker} from "react-native-maps";
+
+const MyStationCard = ({details, navigation}) => {
+    const [popupVisible, setPopupVisible] = useState(false);
+    const [mapPopupVisible, setMapPopupVisible] = useState(false);
+
+    const openQRPopup = () => {
+        setPopupVisible(true);
+    };
+
+    const closeQRPopup = () => {
+        setPopupVisible(false);
+    };
+
+    const openMapPopup = () => {
+        setMapPopupVisible(true);
+    };
+
+    const closeMapPopup = () => {
+        setMapPopupVisible(false);
+    };
+
+    return (
+        <View style={styles.mainContainer}>
+            <View style={styles.detailsContainer}>
+                {/*Icon and name*/}
+                <View style={styles.rowView}>
+                    <FontAwesome5 name="charging-station" size={20} color="black"/>
+                    <Text style={styles.name}>{details.name}</Text>
+                </View>
+
+                {/*Charging status*/}
+                <View style={styles.rowView}>
+                    <Text style={[
+                        styles.regularText,
+                        details.status === "CHARGING" ? {color: 'green', fontWeight: 'bold'} : null
+                    ]}>
+                        {
+                            details.status === "CHARGING" ? "In use" : "Available"
+                        }
+                    </Text>
+                </View>
+
+                {/*Charging type*/}
+                <View style={styles.rowView}>
+                    <Text style={styles.regularText}>
+                        {
+                            details.type === "TYPE_0" ? "Type 0" : "Type 1"
+                        }
+                    </Text>
+                </View>
+
+                {/*Price*/}
+                <View style={styles.rowView}>
+                    <Text style={styles.regularText}>{details.price}₪ (per kWh)</Text>
+                </View>
+
+                {/*Rating - if no value, print message*/}
+                <View style={styles.rowView}>
+                    {
+                        details.avgRat > 0 ?
+                            <StarRating style={styles.regularText} starSize={17} rating={details.avgRat}
+                                        onChange={(newGrade) => null}/>
+                            : <Text style={styles.regularText}>No rating available.</Text>
+
+                    }
+                </View>
+            </View>
+
+            {/*Left panel Buttons*/}
+            <View style={styles.buttonContainer}>
+                <Button style={styles.button} title={"Watch QR Code"} onPress={ openQRPopup }/>
+                <Button style={styles.button} title={"Edit Station"} onPress={ () => navigation.navigate("EditStation", { stationId: details.id} )}/>
+                <Button style={styles.button} title={"See Location"} onPress={ openMapPopup}/>
+                <Button style={styles.button} title={"*Delete Station"}/>
+            </View>
+
+            {/*Popups!*/}
+            <Popup
+                visible={popupVisible}
+                onClose={closeQRPopup}
+                navigation={null}
+                txt = <QRCode value= {details.id} size={300}/>
+                navigateTo={null}
+            />
+
+            <Popup
+                visible={mapPopupVisible}
+                onClose={closeMapPopup}
+                navigation={null}
+                txt = {<MapView
+                    style={styles.map}
+                    region={{longitude:details.location.longitude, latitude:details.location.latitude}}
+                    initialRegion={{longitude:details.location.longitude, latitude:details.location.latitude, latitudeDelta: 0.001,
+                        longitudeDelta: 0.001,}}
+                    scrollEnabled={false}
+                    zoomEnabled={false}
+                    rotateEnabled={false}>
+                    <Marker coordinate={{
+                        latitude: details.location.latitude,
+                        longitude: details.location.longitude
+                    }}/>
+                </MapView>}
+            navigateTo={null}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    mapContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    map: {
+        width: Dimensions.get("window").width,
+        height: 250
+    },
+    mainContainer: {
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 10,
+        flexDirection: "row",
+    },
+    detailsContainer: {
+        flex: 1,
+        padding: 10,
+        justifyContent: 'space-between'
+    },
+    buttonContainer: {
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    name: {
+        fontWeight: "bold",
+        marginLeft: 10
+    },
+    regularText: {
+        marginLeft: 33,
+        fontSize:14
+    },
+    rowView: {
+        flexDirection: 'row',
+    },
+    button: {
+        marginBottom: 10,
+        justifyContent: 'space-between',
+    }
+});
+
+export default MyStationCard;
--- ui-rn6/chargeIT/src/screens/mainFlow/MyStations.js
+++ ui-rn6/chargeIT/src/screens/mainFlow/MyStations.js
@@ -1,168 +1,167 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useIsFocused, useFocusEffect } from "@react-navigation/native";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, {useState, useEffect, useContext} from "react";
+import {useIsFocused} from "@react-navigation/native";
+import {View, Text, StyleSheet} from "react-native";
 import Buttons from "../../components/Buttons";
 import Spacer from "../../components/Spacer";
-import { Context as StationsContext } from "../../context/StationsContext";
-import { ScrollView } from "react-native";
+import {Context as StationsContext} from "../../context/StationsContext";
+import {ScrollView} from "react-native";
+import MyStationCard from "../../components/MyStationCard";
 
-const MyStations = ({ navigation }) => {
-  const { state, getAllStationsByUser } = useContext(StationsContext);
-  const [areStationsAvailable, setAreStationsAvailable] = useState(false);
-  const [usersStationsAvailable, setUsersStationsAvailable] = useState(null);
+const MyStations = ({navigation}) => {
+    const {state, getAllStationsByUser} = useContext(StationsContext);
+    const [areStationsAvailable, setAreStationsAvailable] = useState(false);
+    const [usersStationsAvailable, setUsersStationsAvailable] = useState(null);
 
-  const isFocused = useIsFocused();
+    const isFocused = useIsFocused();
 
-  // Check if there are stations available for the user.
-  useEffect(() => {
-    const calculateBooleanArgument = async () => {
-      try {
-        const result = await getAllStationsByUser();
-        if (result.length > 0) {
-          setAreStationsAvailable(true);
-          setUsersStationsAvailable(result);
-          console.log(result);
-        } else {
-          setAreStationsAvailable(false);
+    // Check if there are stations available for the user.
+    useEffect(() => {
+        const calculateBooleanArgument = async () => {
+            try {
+                const result = await getAllStationsByUser();
+                if (result.length > 0) {
+                    setAreStationsAvailable(true);
+                    setUsersStationsAvailable(result);
+                    console.log(result);
+                } else {
+                    setAreStationsAvailable(false);
+                }
+            } catch (error) {
+                console.log("Error fetching data:", error);
+            }
+        };
+
+        if (isFocused) {
+            calculateBooleanArgument();
         }
-      } catch (error) {
-        console.log("Error fetching data:", error);
-      }
+    }, [isFocused]);
+    //
+
+    const renderStation = (station) => {
+        const stationDetails = JSON.parse(Object.values(station)[0]);
+        const {id, location, status, pricePerVolt, chargerType, stationName, reviews} = stationDetails;
+
+        const avgRat = reviews.reduce((sum, review) => sum + review.grade, 0) / reviews.length;
+
+        return (
+            <View key={id} style={{margin: 10}}>
+                <MyStationCard details={{
+                    name: stationName,
+                    type: chargerType,
+                    price: pricePerVolt,
+                    status: status,
+                    location: location,
+                    avgRat: avgRat,
+                    id: id
+                }} navigation={navigation}/>
+            </View>
+        );
     };
 
-    if (isFocused) {
-      calculateBooleanArgument();
+    if (areStationsAvailable) {
+        if (usersStationsAvailable && usersStationsAvailable.length > 0) {
+            return (
+                <View style={styles.mainView}>
+                    <Text style={styles.title}>Manager your Stations</Text>
+                    <ScrollView contentContainerStyle={styles.scroll}>
+                        {usersStationsAvailable.map(renderStation)}
+                    </ScrollView>
+                    <View style={styles.buttonViewContainer}>
+                        <Buttons
+                            btn_text={" Create another station "}
+                            on_press={() => navigation.navigate("SelectLocation")}
+                        />
+                    </View>
+                </View>
+            );
+        }
+    } else {
+        return (
+            <View style={styles.mainView}>
+                <Spacer></Spacer>
+                <Text style={styles.noStationsText}>
+                    You currently don't have any stations.
+                </Text>
+                <Text style={styles.noStationsText}>Want to make money?</Text>
+                <Spacer></Spacer>
+                <View style={styles.buttonViewContainer}>
+                    <Buttons
+                        btn_text={"Create Station"}
+                        on_press={() => navigation.navigate("SelectLocation")}
+                    />
+                </View>
+            </View>
+        );
     }
-  }, [isFocused]);
-  //
-
-  const renderStation = (station) => {
-    const stationDetails = JSON.parse(Object.values(station)[0]);
-    const { id, location, status, pricePerVolt, chargerType, stationName } =
-      stationDetails;
-
-    return (
-      <TouchableOpacity
-        key={id}
-        onPress={() => navigation.navigate("EditStation", { stationId: id })}
-      >
-        <View key={id} style={styles.wrapper}>
-          <View style={styles.stationContainer}>
-            <Text style={styles.stationName}>{stationName}</Text>
-            <Text style={styles.stationStatus}>
-              Charger type: {chargerType}
-            </Text>
-            <Text style={styles.stationStatus}>
-              Status: {status === "NOT_CHARGING" ? "Ready for use" : "Charging"}
-            </Text>
-            <Text style={styles.stationStatus}>
-              Price per Volt: {pricePerVolt}$
-            </Text>
-          </View>
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  if (areStationsAvailable) {
-    if (usersStationsAvailable && usersStationsAvailable.length > 0) {
-      return (
-        <View style={styles.mainView}>
-          <Text style={styles.title}>My Stations</Text>
-          <ScrollView contentContainerStyle={styles.scroll}>
-            <View>{usersStationsAvailable.map(renderStation)}</View>
-          </ScrollView>
-          <View>
-            <Buttons
-              btn_text={" Create another station "}
-              on_press={() => navigation.navigate("SelectLocation")}
-            />
-          </View>
-        </View>
-      );
-    }
-  } else {
-    return (
-      <View style={styles.mainView}>
-        <Spacer></Spacer>
-        <Text style={styles.noStationsText}>
-          You currently don't have any stations.
-        </Text>
-        <Text style={styles.noStationsText}>Want to make money?</Text>
-        <Spacer></Spacer>
-        <Buttons
-          btn_text={"Create Station"}
-          on_press={() => navigation.navigate("SelectLocation")}
-        />
-      </View>
-    );
-  }
 };
 
 const styles = StyleSheet.create({
-  mainView: {
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-    flexDirection: "column",
-    marginTop: 100,
-  },
-  title: {
-    textAlign: "center",
-    fontSize: 23,
-    textShadowColor: "gray",
-  },
-  noStationsText: {
-    fontSize: 18,
-    fontStyle: "italic",
-    fontWeight: "400",
-  },
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  stationContainer: {
-    padding: 10,
-    marginBottom: 10,
-    maxWidth: 350,
-    justifyContent: "center",
-    textAlign: "center",
-    justifyItems: "center",
-    borderRight: "4 bold",
-  },
-  stationName: {
-    fontSize: 16,
-    fontWeight: "bold",
-    paddingBottom: 15,
-  },
-  stationLocation: {
-    marginTop: 5,
-    fontSize: 14,
-  },
-  stationStatus: {
-    marginTop: 5,
-    fontSize: 14,
-    color: "gray",
-    fontWeight: "bold",
-  },
-  scroll: {
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
+    mainView: {
+        justifyContent: "center",
+        flex: 1,
+        flexDirection: "column",
+    },
+    title: {
+        textAlign: "center",
+        fontSize: 23,
+        textShadowColor: "gray",
+    },
+    noStationsText: {
+        fontSize: 18,
+        fontStyle: "italic",
+        fontWeight: "400",
+    },
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    stationContainer: {
+        padding: 10,
+        marginBottom: 10,
+        maxWidth: 350,
+        justifyContent: "center",
+        textAlign: "center",
+        justifyItems: "center",
+        borderRight: "4 bold",
+    },
+    stationName: {
+        fontSize: 16,
+        fontWeight: "bold",
+        paddingBottom: 15,
+    },
+    stationLocation: {
+        marginTop: 5,
+        fontSize: 14,
+    },
+    stationStatus: {
+        marginTop: 5,
+        fontSize: 14,
+        color: "gray",
+        fontWeight: "bold",
+    },
+    scroll: {
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
 
-  wrapper: {
-    flex: 1,
-    flexDirection: "row",
-    alignContent: "space-between",
-    marginTop: 30,
-    padding: 10,
-    color: "white",
-    width: 300,
-    maxHeight: 150,
-    borderColor: "white",
-    borderRadius: 6,
-    borderWidth: 2,
-  },
+    wrapper: {
+        flex: 1,
+        flexDirection: "row",
+        alignContent: "space-between",
+        marginTop: 30,
+        padding: 10,
+        color: "white",
+        width: 300,
+        maxHeight: 150,
+        borderColor: "white",
+        borderRadius: 6,
+        borderWidth: 2,
+    },
+
+    buttonViewContainer: {
+        alignContent:'center',
+        alignItems:'center'
+    }
 });
 
 export default MyStations;
